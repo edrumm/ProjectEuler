968cbb296225397899f7b0161d507d77d74ed157
edrumm <esd0658@googlemail.com>
2020-01-07 22:25:18 +0000

    started #4

--- src/Problem4.java
+++ src/Problem4.java
@@ -7,8 +7,27 @@ public class Problem4 {
      * Find the largest palindrome made from the product of two 3-digit numbers
      */
 
+    // TODO:
     public int run() {
+        return calculate();
+    }
+
+    private int calculate() {
         return 0;
     }
 
+    private int isPalidrome(int n) {
+        return 0;
+    }
+
+    private int reverse(int n) {
+        int reversed = 0;
+
+        while (n != 0) {
+            // TODO
+            // ...
+        }
+
+        return reversed;
+    }
 }
