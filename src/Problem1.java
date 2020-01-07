b44b5952ff7452d1c30b541bd3de2d31c2533aff
edrumm <esd0658@googlemail.com>
2020-01-07 17:16:27 +0000

    first commit

---
+++ src/Problem1.java
@@ -0,0 +1,27 @@
+public class Problem1 {
+
+    /*
+     * ProjectEuler problem 1
+     * Multiples of 3 and 5
+     *
+     * Find the sum of all multiples of 3 or 5 below 1,000
+     */
+
+    static int multiples() {
+        int sum = 0;
+
+        for (int i = 0; i < 1000; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
+
+}
---
+++ src/Problem2.java
@@ -0,0 +1,37 @@
+public class Problem2 {
+
+    /*
+     * ProjectEuler problem 1
+     * Even Fibonacci numbers
+     *
+     * Find the sum of all even Fibonacci numbers up to 4,000,000
+     */
+
+    static boolean isEven(int n) {
+
+        return (n % 2 == 0);
+
+    }
+
+    static int fibonacci() {
+        int sum = 0;
+        int a = 1, b = 2, ab = 0;
+
+        while (a < 4000000) {
+
+            if (isEven(a)) {
+                sum += a;
+            }
+
+            ab = a + b;
+            a = b;
+            b = ab;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci());
+    }
+}
