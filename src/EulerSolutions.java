f04c48c2f69a3eea8109f7b0e3df5dbb60095b34
edrumm <esd0658@googlemail.com>
2020-01-18 23:51:16 +0000

    updated solutions, started 012

--- src/EulerSolutions.java
+++ src/EulerSolutions.java
@@ -1,16 +1,19 @@
 public class EulerSolutions {
 
     public static void main(String[] args) {
-        System.out.println("01: " + new Problem001().run());
-        System.out.println("02: " + new Problem002().run());
-        System.out.println("03: " + new Problem003().run());
-        System.out.println("04: " + new Problem004().run());
-        System.out.println("05: " + new Problem005().run());
-        System.out.println("06: " + new Problem006().run());
-        System.out.println("07: " + new Problem007().run());
-        System.out.println("08: " + new Problem008().run());
-        System.out.println("09: " + new Problem009().run());
-        System.out.println("10: " + new Problem010().run());
-        System.out.println("11: " + new Problem011().run());
+        // UNCOMMENT TO RUN :)
+        // System.out.println("01: " + new Problem001().run());
+        // System.out.println("02: " + new Problem002().run());
+        // System.out.println("03: " + new Problem003().run());
+        // System.out.println("04: " + new Problem004().run());
+        // System.out.println("05: " + new Problem005().run());
+        // System.out.println("06: " + new Problem006().run());
+        // System.out.println("07: " + new Problem007().run());
+        // System.out.println("08: " + new Problem008().run());
+        // System.out.println("09: " + new Problem009().run());
+        // System.out.println("10: " + new Problem010().run());
+        // System.out.println("11: " + new Problem011().run());
+        System.out.println("12: " + new Problem012().run());
+
     }
 }
---
+++ src/Problem012.java
@@ -0,0 +1,16 @@
+public class Problem012 {
+
+    /*
+     * ProjectEuler problem 12
+     * Highly divisible triangular number
+     *
+     * What is the value of the first triangle number to have over
+     * five hundred divisors?
+     */
+
+    public int run() {
+        // TODO:
+        return 0;
+    }
+
+}
