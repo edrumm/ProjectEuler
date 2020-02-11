11732a49a7a671c76f582509b248dfd4ef4a7b03
edrumm <esd0658@googlemail.com>
2020-02-11 00:14:37 +0000

    started 017

---
+++ src/Problem017.java
@@ -0,0 +1,22 @@
+public class Problem017 {
+
+    /*
+     * ProjectEuler problem 17
+     * Number letter counts
+     *
+     * If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words,
+     * how many letters would be used?
+     */
+
+    private String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven",
+                                "eight", "nine", "eleven", "twelve", "thirteen", "fourteen",
+                                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private String[] tens = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    public int run() {
+
+        // TODO
+
+        return 0;
+    }
+}
